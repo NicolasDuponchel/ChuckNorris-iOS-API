9b5f94b842397be6e5b4f204a5d24b304a47cfa1
nicolas_duponchel <nduponchel@oui.sncf>
2018-06-20 18:56:22 +0200

    Initial Commit

---
+++ First API call/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First API call
+//
+//  Created by Duponchel Nicolas on 20/06/2018.
+//  Copyright © 2018 Duponchel Nicolas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
